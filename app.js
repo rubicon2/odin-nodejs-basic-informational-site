34a05745023f4eaed34e01ccf7cec1c41fa1ecf4
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-08-14 11:05:35 +0100

    Implement contact-me page content

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = new express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.listen(8080, () => console.log('Listening for requests on port 8080'));
 
 // Routes
@@ -17,6 +18,12 @@ app.get('/about', (req, res) => {
 
 app.get('/contact-me', (req, res) => {
   res.render('contact-me', { title: 'Contact Me' });
+});
+
+app.post('/contact-me', (req, res) => {
+  // Pretend to send off the form values
+  console.log('Received form values: ', req.body);
+  res.render('contact-me-success', { title: 'Contact Me' });
 });
 
 app.use((req, res) => {
